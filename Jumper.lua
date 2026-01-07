b7d1b4cf040ba10cf0ecfe590f5fcfe780dc7c8b
solarastuff <brendan.p.stewart@icloud.com>
2026-01-07 15:28:46 -0500

    Update Jumper.lua

--- Jumper.lua
+++ Jumper.lua
@@ -1,5 +1,15 @@
-local uis = game:GetService("UserInputService") 
+local cloneref = cloneref or function(x) return x end
+local uis = cloneref(game:GetService("UserInputService"))
+if not isfolder("JumperV2") then
+cloneref(game:GetService("StarterGui")):SetCore("SendNotification", {
+        Title = "Welcome to the Game!"; -- Title text
+        Text = "Enjoy your stay, " .. LocalPlayer.DisplayName .. "!"; -- Main message
+        Icon = "rbxassetid://10022562411"; -- Optional: Asset ID for an icon (replace with your own)
+        Duration = 5 -- How long it stays (in seconds)
+    })
+end)
 
+end
 if uis.TouchEnabled then
  loadstring(game:HttpGet("https://raw.githubusercontent.com/solarastuff/sorryjimpee/refs/heads/main/JumperMobile.lua"))()
 end
